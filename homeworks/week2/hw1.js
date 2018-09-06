951567912ef73a6c95f28ca3814db73ab806dc87
lin <ly001522@gmail.com>
2018-09-06 22:16:34 +0800

    week2 hw1, hw2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,24 @@
+/*
+// 1
 function stars(n) {
+	var star = "";
+	var stars = [];
+	for(var i=0; i<n; i++){
+		stars.push(star += "*");
+	}
+	return stars;
+}
 
+module.exports = stars;
+*/
+
+// 2
+function stars(n) {
+	var stars = [];
+	for(var i=0; i<n; i++){
+    	stars.push("*".repeat(i+1));
+	}
+	return stars;
 }
 
 module.exports = stars;
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,23 @@
+/*
+// 1
 function alphaSwap(str) {
+	var strArr = str.split("");
+	var newStr = [];
+	for(var i=0; i<strArr.length; i++){
+		var letter = strArr[i];
+		newStr.push(letter===letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase());
+	}
+	return newStr.join("");
+}
 
+module.exports = alphaSwap
+*/
+
+// 2
+function alphaSwap(str) {
+	return str.split("").map(n => {
+		return n===n.toUpperCase() ? n.toLowerCase() : n.toUpperCase();
+	}).join("");
 }
 
 module.exports = alphaSwap
