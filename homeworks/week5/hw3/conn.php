e4fbe99a2ce061d65e7bd1774b438f106de5e105
zzly00 <31249335+zzly00@users.noreply.github.com>
2018-10-03 14:57:37 +0800

    Update conn.php

--- homeworks/week5/hw3/conn.php
+++ homeworks/week5/hw3/conn.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = '166.62.28.131';
-    $username = 'student2nd';
-    $password = 'mentorstudent123';
-    $dbname = 'mentor_program_db';
+    $servername = '';
+    $username = '';
+    $password = '';
+    $dbname = '';
 
     $conn = new mysqli($servername, $username, $password, $dbname);
     mysqli_query($conn, 'SET NAMES UTF8');
